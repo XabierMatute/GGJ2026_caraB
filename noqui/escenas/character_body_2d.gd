6abbf14ffe245df45fdb42078155be9c3a7c12f4
xmatute- <xmatute-@student.42urduliz.com>
2026-01-31 19:38:12 +0100

    xambio de piso

--- noqui/escenas/character_body_2d.gd
+++ noqui/escenas/character_body_2d.gd
@@ -4,6 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0 * 3
 const JUMP_VELOCITY = -400.0
 
+var current_floor = 0
+
+var last_direction_was_left = false
+
+var plantas = {}
+
+func _ready() -> void:
+	plantas = {
+	2 : $"../Planta+2",
+	1 : $"../Planta+1",
+	0 : $"../Planta0",
+	-1: $"../Planta-1",
+	-2: $"../Planta-2",
+	}
+	position.y = plantas[current_floor].position.y
+	
+func change_floor_to(floor :int):
+	current_floor = floor
+	position.y = plantas[current_floor].position.y
+	
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,11 +44,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	if velocity.x < 0:
-		$AnimatedSprite2D.play("walk_left")
+		$AnimatedSprite2D.play("M" + str(current_floor) + "_walk_left")
+		last_direction_was_left = true
 	elif velocity.x > 0:
-		$AnimatedSprite2D.play("walk_right")
+		$AnimatedSprite2D.play("M" + str(current_floor) + "_walk_right")
+		last_direction_was_left = false
 	else:
-		$AnimatedSprite2D.pause() #idle
-		
-
+		if last_direction_was_left:
+			$AnimatedSprite2D.play("M" + str(current_floor) + "_idle_left") #left right
+		else :
+			$AnimatedSprite2D.play("M" + str(current_floor) + "_idle_right") #left right
+			
 	move_and_slide()
+
+
+func _on_button_pressed() -> void:
+	change_floor_to(current_floor + 1)
+
+
+func _on_button_2_pressed() -> void:
+	change_floor_to(current_floor - 1)
