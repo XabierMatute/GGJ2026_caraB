59f561c94d6eb625f352f37aa27b8f3fed4e2ad8
xmatute- <xmatute-@student.42urduliz.com>
2026-01-31 18:35:50 +0100

    estable se supone

--- noqui/escenas/piso_0_lobby.gd
+++ noqui/escenas/piso_0_lobby.gd
@@ -10,10 +10,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		var tween: Tween = create_tween()
+		tween.tween_property($"../../Camera2D", "position", global_position, 1)
+		tween.parallel().tween_property($"../../Camera2D", "zoom", Vector2.ONE / 5, 2)
+		tween.parallel().tween_property(self, "modulate", Color.WHITE, 1)
+
+
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "Player":
+		var tween: Tween = create_tween()
+		tween.parallel().tween_property(self, "modulate", Color("#616161"), 1)
+		#tween.parallel().tween_property($"../../Camera2D", "zoom", Vector2.ONE / 2, 1)
